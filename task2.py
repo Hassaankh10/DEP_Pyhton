9a0577f04bab548844458237b2fea509d13add62
Hassaankh10 <m.hassaan10.mh10@gmail.com>
2024-07-21 15:45:50 +0500

    updated task 2 for user input

--- task2.py
+++ task2.py
@@ -2,17 +2,23 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import ssl
 import csv
-csv_file = open("wiki_links.csv", "w")
+csv_file = open("links.csv", "w")
 context = ssl.create_default_context()
 context.check_hostname = False
 context.verify_mode = ssl.CERT_NONE
-uh = urlopen("https://www.google.com/search?client=safari&rls=en&q=google+search+python&ie=UTF-8&oe=UTF-8",context=context)
+url = input("Enter the url: ")
+uh = urlopen(url ,context=context)
 data = uh.read().decode()
 data = BeautifulSoup(data, "html.parser")
 # print(data.title)
+csv_file.write("Links\n")
 links = data.find_all("a")
 for link in links:
     link = link.get("href").encode()
+    # linkpos = link.find(b"/url?q=")
     link = str(link)
-    csv_file.write(link + "\n")
+    link = link.replace("b'", "")
+    if link.startswith("https://"):
+        csv_file.write(link + "\n")
+        continue
     
